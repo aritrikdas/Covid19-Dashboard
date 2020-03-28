074deeb98dd206994647399f8ede4da8b18c0458
Aritrik <aritrikdas@gmail.com>
2020-03-29 00:39:23 +0530

    Initial commit.

---
+++ src/app/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      {/* footer content */}
+      <footer>
+        <div className="pull-right"> Avoid going out during the lockdown. Help break the chain of spread.</div>
+        <div className="clearfix" />
+      </footer>
+      {/* /footer content */}
+    </div>
+  );
+};
+
+export default Footer;
