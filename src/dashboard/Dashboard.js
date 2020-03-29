8e81a6d2430b49f4102a4800b092bb5b2ded9d2f
tapajyotigiri <tapajyoti.giri@gmail.com>
2020-03-29 19:38:51 +0530

    Adding top counter panel

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import VisitorLocation from "./Visitor_Location"
+import TopCounter from "./TopCounterPanel"
 
 export default class Dashboard extends Component {
     render() {
@@ -7,39 +8,7 @@ export default class Dashboard extends Component {
             <div>
                 <div className="right_col" role="main">
                     {/* top tiles */}
-                    <div className="row tile_count">
-                        <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                            <span className="count_top"><i className="fa fa-user" /> Total Users</span>
-                            <div className="count">2500</div>
-                            <span className="count_bottom"><i className="green">4% </i> From last Week</span>
-                        </div>
-                        <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                            <span className="count_top"><i className="fa fa-clock-o" /> Average Time</span>
-                            <div className="count">123.50</div>
-                            <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc" />3% </i> From last Week</span>
-                        </div>
-                        <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                            <span className="count_top"><i className="fa fa-user" /> Total Males</span>
-                            <div className="count green">2,500</div>
-                            <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc" />34% </i> From last Week</span>
-                        </div>
-                        <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                            <span className="count_top"><i className="fa fa-user" /> Total Females</span>
-                            <div className="count">4,567</div>
-                            <span className="count_bottom"><i className="red"><i className="fa fa-sort-desc" />12% </i> From last Week</span>
-                        </div>
-                        <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                            <span className="count_top"><i className="fa fa-user" /> Total Collections</span>
-                            <div className="count">2,315</div>
-                            <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc" />34% </i> From last Week</span>
-                        </div>
-                        <div className="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-                            <span className="count_top"><i className="fa fa-user" /> Total Connections</span>
-                            <div className="count">7,325</div>
-                            <span className="count_bottom"><i className="green"><i className="fa fa-sort-asc" />34% </i> From last Week</span>
-                        </div>
-                    </div>
-                    
+                    <TopCounter></TopCounter>
                     {/* /top tiles */}
                    
                     {/*<div className="row">
