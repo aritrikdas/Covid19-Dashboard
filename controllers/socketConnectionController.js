644fd03172f6dc8e16895ec06a22492e704e3265
Aritrik <aritrikdas@gmail.com>
2020-03-31 10:49:27 +0530

    Fix syntax error in twitter.

--- controllers/socketConnectionController.js
+++ controllers/socketConnectionController.js
@@ -11,14 +11,18 @@ exports.respond = function (socket) {
     getLatestTweet(socket, 10);
     // setInterval(() => getLatestTweet(socket), 15000);
      
-    twit.stream('statuses/filter', { track: 'covid19', language: 'en', filter_level: 'medium'  }, function (stream) {
+    twit.stream('statuses/filter', { track: 'covid19', language: 'en', filter_level: 'medium' }, function (stream) {
         stream.on('data', function (data) {
             //io.sockets.emit('tweet', data.text);
             socket.emit("FromAPI", data); 
-            console.log('.', data.text);
+            console.log('tweet received', data.text);
         });
 
         stream.on('error', function (err) {
+            console.log("conn err", err);
+        })
+
+        stream.on('disconnect', function (err) {
             console.log("conn err", err);
         })
     });
@@ -28,11 +32,13 @@ exports.respond = function (socket) {
 
 getLatestTweet = (socket, limit) => {
     twit.get('search/tweets', { q: 'covid19', count: limit ? limit : 1, lang: 'en', result_type: 'popular'}, function(err, data, response) {
-        console.log(data.search_metadata);
+        
         if (limit) {
-            socket.emit("FromAPI", data); 
+            socket.emit("FromAPI", data);
+            console.log("Initial tweets received")
         } else {
             socket.emit("FromAPI", data.statuses[0]); 
+            console.log("Initial tweets received");
         }
     })
 }
