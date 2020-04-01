3fb08d9711b8dc062065e91e5777ca691c8b0be9
Aritrik <aritrikdas@gmail.com>
2020-04-02 00:22:28 +0530

    Fix world API data return format.

--- controllers/countryApi.js
+++ controllers/countryApi.js
@@ -1,32 +1,14 @@
 const urlService = require('../services/requestService');
 
-exports.getAllCountriesStat = async function (req,res) {
+exports.getAllCountriesStat = async function (req, res) {
     let returnObj = {};
-    var options = {
-        host: "corona.lmao.ninja",
-        path: "/countries?sort=cases",
-        port: 443,
-        headers: {
-            "Content-Type": "application/json"
-        }
-    };
-
-    urlService.getCall(options, (err, resp) => {
-        let countryDetailsObj = [];
-        if (err) {
-            returnObj.status = false;
-            returnObj.message = {"errMsg" : err};
-            return;
-        } 
-        countryDetailsObj = JSON.parse(resp);
-            returnObj.status = true;
-            returnObj.respObj = countryDetailsObj;
-        //console.log(JSON.parse(resp).length);
-        res.json(returnObj);
-    }); 
+    let worldDetailsObj = JSON.parse(await fetchDataFromAPI("https://corona.lmao.ninja/countries?sort=cases"));
+    returnObj.status = true;
+    returnObj.respObj = worldDetailsObj;
+    res.json(returnObj);
 }
 
-exports.totalStat = async function (req,res) {
+exports.totalStat = async function (req, res) {
     let returnObj = {};
     var options = {
         host: "corona.lmao.ninja",
@@ -41,18 +23,18 @@ exports.totalStat = async function (req,res) {
         let countryDetailsObj = [];
         if (err) {
             returnObj.status = false;
-            returnObj.message = {"errMsg" : err};
+            returnObj.message = { "errMsg": err };
             return;
-        } 
+        }
         countryDetailsObj = JSON.parse(resp);
-            returnObj.status = true;
-            returnObj.respObj = countryDetailsObj;
+        returnObj.status = true;
+        returnObj.respObj = countryDetailsObj;
         //console.log(JSON.parse(resp).length);
         res.json(returnObj);
     });
 }
 
-exports.indiaData = async function (req,res) {
+exports.indiaData = async function (req, res) {
     let returnObj = {};
     var options = {
         host: "api.covid19india.org",
@@ -67,12 +49,12 @@ exports.indiaData = async function (req,res) {
         let countryDetailsObj = [];
         if (err) {
             returnObj.status = false;
-            returnObj.message = {"errMsg" : err};
+            returnObj.message = { "errMsg": err };
             return;
-        } 
+        }
         countryDetailsObj = JSON.parse(resp);
-            returnObj.status = true;
-            returnObj.respObj = countryDetailsObj;
+        returnObj.status = true;
+        returnObj.respObj = countryDetailsObj;
         //console.log(JSON.parse(resp).length);
         res.json(returnObj);
     });
